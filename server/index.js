9412e940f09905295df348622b4b844fdbb3e1cd
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-08-07 20:40:51 +0200

    lazy load get req

--- server/index.js
+++ server/index.js
@@ -1,17 +1,27 @@
-const express = require ('express')
-const cors = require ('cors')
-const booksData = require ('../client/src/books.json')
+const express = require("express");
+const cors = require("cors");
+const booksData = require("../client/src/books.json");
 
-const app = express()
+const app = express();
 app.use(cors());
 
-app.get('/random-book', (req,res)=>{
-    const randomIndex = Math.floor(Math.random()* booksData.length)
-    const randomBook = booksData[randomIndex]
-    res.json(randomBook)
-})
+function getRandomBook(){
+  const randomIndex = Math.floor(Math.random() * booksData.length);
+  const randomBook = booksData[randomIndex];
+  return randomBook;
+};
 
-const port = process.env.PORT||4040;
-app.listen(port,()=>{
-    console.log(`Server is listening port ${port}`);
-})
+app.get("/random-book", (req, res) => {
+  res.json(getRandomBook());
+});
+
+app.get("/random-book-delayed", (req, res) => {
+  setTimeout(() => {
+    res.json(getRandomBook());
+  }, 1000);
+});
+
+const port = process.env.PORT || 4040;
+app.listen(port, () => {
+  console.log(`Server is listening port ${port}`);
+});
